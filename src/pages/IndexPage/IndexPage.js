e9108785029364e13db1b6f86b5c6718909804bd
Hector Parra <hector@parra.cat>
2018-10-15 04:46:11 +0200

    Add typed subtitle

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typed from 'react-typed';
 import Typography from '@material-ui/core/Typography';
 
 import './IndexPage.scss';
@@ -38,17 +39,32 @@ export default class {
 }
 `.trim();
 
+const subtitles = [
+  "I'm a freelance programmer",
+];
+
 export default class IndexPage extends React.Component {
+
   render() {
     return (
       <div className="IndexPage">
         <Particles className="IndexPage__Particles"/>
         <div className="IndexPage__Content">
           <Typography variant='h1'>Hector Parra</Typography>
-          <Typography variant='h3'>I'm a freelance programmer</Typography>
+          <Typography variant='h3'>
+            <Typed
+              strings={subtitles}
+              startDelay={2000}
+              backDelay={4000}
+              typeSpeed={40}
+              backSpeed={50}
+            />
+          </Typography>
           <div className="IndexPage__Content__Code">
             <Typography variant='h6'>Check my source code</Typography>
-            <CodeWindow title='HectorParra.js' className="IndexPage__Content__Code__Window">{code}</CodeWindow>
+            <CodeWindow title='HectorParra.js' className="IndexPage__Content__Code__Window">
+              {code}
+            </CodeWindow>
           </div>
         </div>
       </div>
